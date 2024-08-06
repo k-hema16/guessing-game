49af29b66ac37e95e53a33f3c77a3810b9283f5e
k-hema16 <hemakondur16@gmail.com>
2024-08-06 23:17:27 +0530

    Update guess.js

--- guess.js
+++ guess.js
@@ -1,25 +1,26 @@
 let guessNumberElement = document.getElementById("guessNumber");
-let randomNum = Math.ceil(Math.random() * 100);
+let randomNum = Math.ceil(Math.random() * 100);  //Creating a random number
 console.log(randomNum);
 let gameResult = document.getElementById("gameResult");
 let reStartBtn=document.getElementById("reStartBtn");
 let count=0;
 let scoreEle=document.getElementById("scoreEle");
 
+//Function to check whether the input matches with given number
 function checkGuess() {
     let guessedNumber = parseInt(guessNumberElement.value);
     if (guessedNumber > randomNum) {
-        gameResult.textContent = "Too High,Try again!!";
+        gameResult.textContent = "Too High,Try again!!";     
         gameResult.style.backgroundColor = "#1e217c"
     } else if (guessedNumber < randomNum) {
-        gameResult.textContent = "Too Low,Try again!!";
+        gameResult.textContent = "Too Low,Try again!!";     
         gameResult.style.backgroundColor = "#1e217c"
     } else if (guessedNumber === randomNum) {
-        gameResult.textContent = "Congragulations!! You got it right.";
-        randomNum = Math.ceil(Math.random() * 100);
+        gameResult.textContent = "Congragulations!! You got it right.";    
+        randomNum = Math.ceil(Math.random() * 100);               //If Gussed Number is Correct it creates New Random Number
         console.log(randomNum);
         count+=1
-        scoreEle.textContent=count;
+        scoreEle.textContent=count;                        //Score gets updated
         gameResult.style.backgroundColor = "green"
     } else {
         gameResult.textContent = "Provide a valid input";
